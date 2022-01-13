7fd1acc7ae7acb9fca33360cf266a46e6fac99cc
LucaschS <95489161+LucaschS@users.noreply.github.com>
2022-01-13 09:35:46 +0100

    first commit

--- Index.js
+++ Index.js
@@ -6,13 +6,15 @@ var x = fn(1, 20, 30);
 var y = fn(1, 50, 20);
 var z = fn(2, 3, 4);
 var c = fn(1, 2, 3);
-console.log(x + y + z);
+console.log(x + y + z + c);
 
 const person = {
-    first.name: "Zdzichu",
-    last.name: "Wiesiek",
+    firstName: "Zdzichu",
+    lastName: "Wiesiek",
     id: 5566,
     fullName: function () {
-        return this.firstName + '' + this.lastName;
+        return this.firstName + '  ' + this.lastName;
     }
 };
+
+document.getElementById("root").innerHTML = person.fullName();
